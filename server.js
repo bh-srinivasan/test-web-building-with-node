5fa3d9544b4ecb9a9df211c44acc309569ea8415
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-21 21:48:35 +0530

    1st change

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { dirname } from 'path';
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.resolve('./static')));
+
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(path.resolve('./index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.sendFile(path.resolve('./speakers.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
