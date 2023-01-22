1cf3edee189b334e39796ef54617e30fb69361bf
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-23 00:07:19 +0530

    Change from ES6 Code

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
+
+const routes = require('./routes');
 
 const app = express();
 
 const port = 3000;
 
-const routes = require('./routes');
-
 app.set('view engine', 'ejs');
-app.set('views', path.resolve('./views'));
+app.set('views', path.join(__dirname, './views'));
 
-app.use(express.static(path.resolve('./static')));
+app.use(express.static(path.join(__dirname, './static')));
 
-app.use("/", routes());
+app.use('/', routes());
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Express server listening on port ${port}!`);
 });
