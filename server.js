202a9be8ec8e7aeb481916950ed3e669c341e88e
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-22 22:16:53 +0530

    Using Routes

--- server.js
+++ server.js
@@ -5,21 +5,14 @@ const app = express();
 
 const port = 3000;
 
+const routes = "./routes";
+
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
 app.use(express.static(path.resolve('./static')));
 
-console.log(`Restarting`)
-
-app.get('/', (req, res) => {
-    res.render('pages/index', { pageTitle: 'Welcome' });
-});
-
-app.get('/speakers', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile(path.resolve('./static/speakers.html'));
-});
+app.use("/", routes());
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
