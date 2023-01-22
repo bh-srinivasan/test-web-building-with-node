7e13f35751b319c1e301251093df4185780ad15d
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-22 23:04:13 +0530

    Random Changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = 3000;
 
-const routes = "./routes";
+const routes = require('./routes');
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
