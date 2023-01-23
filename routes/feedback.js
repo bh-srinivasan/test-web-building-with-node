9b9f3eb2a2e9971e596f5b830790b011f81ed56e
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-23 20:09:25 +0530

    No changes1

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,13 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = () => {
-  router.get('/', (request, response) => {
-    return response.send('Feedback page');
-  });
+  router.get('/', (request, response) => response.send('Feedback page'));
 
-  router.post('/', (request, response) => {
-    return response.send('Feedback form posted');
-  });
+  router.post('/', (request, response) => response.send('Feedback form posted'));
 
   return router;
 };
