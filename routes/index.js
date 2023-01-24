4f7777f4ca5ae3904eae5016c58865df64e0c885
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-24 07:46:11 +0530

    added Speaker Routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const speakerRoute = require('./speakers');
+
 const router = express.Router();
 
 module.exports = () => {
   router.get('/', (request, response) => {
     response.render('pages/index', { pageTitle: 'Welcome' });
   });
+    router.get('/speakers', (request, response) => speakerRoute());
+
   return router;
 };
--- routes/speakers.js
+++ routes/speakers.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+console.log('Coming here');
+
 module.exports = () => {
+
   router.get('/', (request, response) => response.send('Speakers list'));
 
   router.get('/:shortname', (request, response) => response.send(`Detail page of ${request.params.shortname}`));
