1e58b84730917c3a41d6e6586128da2ce1084236
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-01-26 09:10:15 +0530

    Trying views changes

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+// Define express and path variables
 const express = require('express');
 const path = require('path');
 
+// Create a variable for Route where different items can be routed
 const routes = require('./routes/index');
 
 const app = express();
 
+// Create a port where we can listen
 const port = 3000;
 
+// Setting express template and views path
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views'));
+// app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
