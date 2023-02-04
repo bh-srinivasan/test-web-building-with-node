80ff359de0b21352b34da2e28cb246964abc0923
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-02-05 02:26:25 +0530

    Display all images dynamically

--- routes/speakers.js
+++ routes/speakers.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+var fs = require('fs');
+var path = require('path');
 
 module.exports = params => {
 
@@ -9,31 +11,45 @@ module.exports = params => {
 
     router.get('/', async (request, response) => {
 
-        /* if (!request.session.visitcount) {
-            request.session.visitcount = 1;
-        } else {
-            request.session.visitcount += 1;
-        }
-        console.log(request.session.visitcount); */
-
         const Speakers = await speakersService.getList();
-        // console.log(Speakers);
-        
+        response.render('layout/index', { pageTitle: 'Speakers', template: 'speakers', Speakers });
+        /* if (!request.session.visitcount) {
+           request.session.visitcount = 1;
+       } else {
+           request.session.visitcount += 1;
+       }
+       console.log(request.session.visitcount); */
+    });
 
-        response.render('layout/index', { pageTitle: 'Speakers', template: 'speakers', Speakers});
-
-        // Commenting below lines to templatize the pages
-        // response.json(speakers);
-
-
-               
-});
-    router.get('/:shortname', (request, response) => response.send(`Detail page of ${request.params.shortname}`));
-
-  /* router.get('/', (request, response) => response.send('Speakers list'));
-
-     */
-
-
-  return router;
-};
+    router.get('/:shortname', async (request, response) => {
+        const speaker = await speakersService.getSpeaker(request.params.shortname)
+        console.log(speaker);
+      
+        // Find image file for respective speaker
+        const speakerName = request.params.shortname;
+        console.log(`Speaker Identified is: ${speakerName}`);
+        const imageFiles = [];
+        console.log(`Directory Identified is: ${path.join(__dirname, '../static/images/artwork')}`)
+      
+        fs.readdir(path.join(__dirname, '../static/images/artwork'), (err, files) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading directory');
+          }
+      
+          files.forEach(file => {
+            console.log(`Checking file: ${file}`);
+            if (file.match(`^${speakerName}.*_tn.jpg$`)) {
+              imageFiles.push(file);
+            }
+          });
+          console.log(`Found imageFiles: ${imageFiles}`);
+      
+          response.render('layout/index', { pageTitle: 'Speakers', template: 'speaker-details', speaker, imageFiles });
+        });
+      });
+      
+      return router;
+      };
+      
+      
