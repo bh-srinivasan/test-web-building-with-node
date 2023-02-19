ccdab95e0f56e176d53762102cebb1c37b763b75
bh-srinivasan <bhsrinivasan@microsoft.com>
2023-02-19 19:18:15 +0530

    Feedback Form Work

--- routes/feedback.js
+++ routes/feedback.js
@@ -2,26 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-
 module.exports = params => {
-    const {feedbackService} = params;
+  const { feedbackService } = params;
 
-    router.get('/', async (request, response,next) => {
-        try{
-        const Feedback = await feedbackService.getList();
-        return response.render('layout/index', { pageTitle: 'Feedback', template: 'feedback',Feedback  });
-        }
-        catch(err){
-          return next(err);
-        }
-        
-       
-    });
+  router.get('/', async (request, response, next) => {
+    try {
+      const feedback = await feedbackService.getList();
+      return response.render('layout', {
+        pageTitle: 'Feedback',
+        template: 'feedback',
+        feedback,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  });
 
-return router;
+  router.post('/', (request, response) => response.send('Feedback form posted'));
+
+  return router;
 };
-    // router.get('/:shortname', (request, response) => response.send(`Detail page of ${request.params.shortname}`));
-
-    /* router.get('/', (request, response) => response.send('Speakers list'));
-  
-       */
